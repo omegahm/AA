2ead9964bdc848be9597aeab36d70b202c2bbce3
Mads Ohm Larsen <mads.ohm@gmail.com>
2011-05-03 00:31:09 +0200

    Almost done, however, I think we might need to add the extra verticies by hand?

--- Project 1/part1.py
+++ Project 1/part1.py
@@ -1,45 +1,47 @@
 import json
 from pulp import *
 
-INF = 10^10
+# Define the number of last source 
+LAST_SOURCE = 7
 
+# Define the number of first sink
+FIRST_SINK = 26
+
+# Lets define the LP-problem
 prob = LpProblem("carsInCopenhagen", LpMaximize)
 
+# We load the problem graph
 with open('CopenhagenGraph.json') as f:
     graph = json.load(f)
 
-variable_array = []
-#variable_array.append(LpVariable('s', 0, INF))
-objective_value = 0
-edges = [None]*30
+# We need to have all the ingoing and all the outgoing edges
+out_edges = [[] for i in range(30)]
+in_edges = [[] for i in range(30)]
 
+# For each e in E | 0 < x_e <= c_e
 for e in graph['edgelist']:
-    if e['u'] <= 6 or e['v'] <= 6:
-        objective_value += e['weight']
-
-    tmp = LpVariable('(' + str(e['u']) + ',' + str(e['v']) + ')', 0, e['weight'])
-    variable_array.append(tmp)
-
-    if edges[e['u']] == None:
-        edges[e['u']] = []
-    edges[e['u']].append(tmp)
-
-    tmp = LpVariable('(' + str(e['v']) + ',' + str(e['u']) + ')', 0, e['weight'])
-    variable_array.append(tmp)
+    tmp = LpVariable('o(' + str(e['u']) + ',' + str(e['v']) + ')', 0, e['weight'])
     
-    if edges[e['v']] == None:
-        edges[e['v']] = []
-    edges[e['v']].append(tmp)
-     
-#for x in edges:
-#    prob += lpSum(x) <= 0
-#    prob += lpSum(x) >= lpSum(x)
+    out_edges[e['u']].append(tmp)
+    in_edges[e['v']].append(tmp)
 
-
-prob += lpSum(variable_array[0:25])
-print edges
     
-#print variable_array
+    tmp = LpVariable('i(' + str(e['u']) + ',' + str(e['v']) + ')', 0, e['weight'])
+    out_edges[e['v']].append(tmp)
+    in_edges[e['u']].append(tmp)
 
+# Objective function
+prob += lpSum(out_edges[0:6]), "Maximum number of cars"    
 
+# For each v in V \ {s,t} | Sum_{e=(u,v) \in E}(x_e) = Sum_{e=(v,w) \in E}(x_e)
+for n in range(LAST_SOURCE, FIRST_SINK):
+    prob += lpSum(in_edges[n]) == lpSum(out_edges[n]), "Input needs to equal output " + str(n)
+        
+prob.writeLP("CopenhagenGraph.lp")
+#print prob
 status = prob.solve(GLPK(msg = 0))
+
+print "Status:", LpStatus[prob.status]
+
+for variable in prob.variables():
+    print variable.name, "=", variable.varValue
